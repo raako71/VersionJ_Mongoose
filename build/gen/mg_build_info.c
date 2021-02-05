f9e7785bcb897373d3622bebe23937f253b8c03c
Harry Parkinson <mail@raako.net>
2021-02-05 17:10:53 +1030

    cleaning up folder stucture

--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20210110-092131/2.18.0-g24c520d";
-const char *mg_build_timestamp = "2021-01-10T09:21:31Z";
-const char *mg_build_version = "2.18.0";
