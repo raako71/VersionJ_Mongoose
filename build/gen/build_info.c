38f77fa7bb255ede5072dbb4975f1502667cd9c9
Harry <mail@raako.net>
2022-09-26 19:25:52 +0930

    remove ignored files (attempt)

--- build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20220317-162150";
-const char *build_timestamp = "2022-03-17T16:21:50Z";
-const char *build_version = "0.10.9.3";
--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20220317-162150/2.20.0-g1885c0f";
-const char *mg_build_timestamp = "2022-03-17T16:21:50Z";
-const char *mg_build_version = "2.20.0";
